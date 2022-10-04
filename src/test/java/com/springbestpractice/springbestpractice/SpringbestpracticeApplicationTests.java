45796b5d4c60f270771c33ccc759cc719bf65a84
Amit <amit.ghosh@cefalo.com>
2022-10-04 08:36:02 -0500

    initial commit

---
+++ src/test/java/com/springbestpractice/springbestpractice/SpringbestpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbestpractice.springbestpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbestpracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
