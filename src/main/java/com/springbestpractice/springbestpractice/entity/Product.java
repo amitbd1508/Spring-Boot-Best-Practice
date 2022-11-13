df3350a0cff1c3d14690c9239bff97490b6d71d7
Amit <amit.ghosh@cefalo.com>
2022-11-13 02:16:23 -0600

    add test case

--- src/main/java/com/springbestpractice/springbestpractice/entity/Product.java
+++ src/main/java/com/springbestpractice/springbestpractice/entity/Product.java
@@ -1,11 +1,15 @@
 package com.springbestpractice.springbestpractice.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.List;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Product {
   @Id
--- src/test/java/com/springbestpractice/springbestpractice/SpringbestpracticeApplicationTests.java
+++ src/test/java/com/springbestpractice/springbestpractice/SpringbestpracticeApplicationTests.java
@@ -1,13 +1,40 @@
 package com.springbestpractice.springbestpractice;
 
+import com.springbestpractice.springbestpractice.entity.Product;
+import com.springbestpractice.springbestpractice.repository.ProductRepository;
+import com.springbestpractice.springbestpractice.service.ProductService;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringbestpracticeApplicationTests {
+  @Autowired
+  private ProductService productService;
+
+  @MockBean
+  private ProductRepository productRepository;
+
 
 	@Test
-	void contextLoads() {
+	public void getProductsTest() {
+    when(productRepository.findAll())
+      .thenReturn(
+        Stream.of(new Product(1, "Product", 2000, 3f, "Physical")).collect(Collectors.toList())
+      );
+    assertEquals(1, productService.findAll().size());
+    verify(productRepository, times(1)).findAll();
 	}
 
 }
